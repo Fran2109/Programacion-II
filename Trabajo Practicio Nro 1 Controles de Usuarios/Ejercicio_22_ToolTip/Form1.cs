0daaad7990d894ff40061f6f7fa49a5203bc1740
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2022-04-12 12:44:58 -0300

    12/04/2022

---
+++ Trabajo Practicio Nro 1 Controles de Usuarios/Ejercicio_22_ToolTip/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_22_ToolTip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            toolTip1.SetToolTip(this.textBox1, "Ingrese un Nombre");
+            toolTip1.SetToolTip(this.textBox2, "Ingrese un Apellido");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text!="" && textBox2.Text!="")
+            {
+                MessageBox.Show("Datos enviados Correctamente");
+                textBox1.Text = textBox2.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Hay datos Vacios");
+            }
+        }
+    }
+}
